35ef1edae22983c238f67d6520a4fc4156f12b23
Eduardo Jimenez <eduardojimenezmedinaEJM@gmail.com>
2023-12-02 13:17:16 -0600

    Initial commit

---
+++ src/main/java/com/corpsy/msdate/MsDateApplication.java
@@ -0,0 +1,13 @@
+package com.corpsy.msdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corpsy/msdate/MsDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corpsy.msdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
